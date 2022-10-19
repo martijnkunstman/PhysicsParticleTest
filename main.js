e65d4b6c71b5cf49950da8e7ecde200862aa73cc
martijnkunstman <martijn@kunstman.nl>
2022-10-20 01:27:13 +0200

    random seed

--- main.js
+++ main.js
@@ -1,36 +1,63 @@
-Math.seedrandom(0);
+function RNG(seed) {
+    var m = 2**35 - 31
+    var a = 185852
+    var s = seed % m
+    return function () {
+        return (s = s * a % m) / m
+    }
+}
+
+Math.random = RNG(1);
+console.log(Math.random() );
+console.log(Math.random() );
+//
 let can = document.getElementById('main');
 let con = can.getContext('2d');
+
+let width = 1200;
+let height = 600;
 const scale = 1;
-function draw (x, y, color) {
+
+function draw(x, y, color) {
     con.fillStyle = color;
     con.fillRect(x - 3, y - 3, 6, 6);
 }
-can.width = window.innerWidth*scale;
-can.height = window.innerHeight*scale;
+can.width = width * scale;
+can.height = height * scale;
 
-let yellow = addParticles(800);
-let red = addParticles(500);
-let green = addParticles(2000);
+let yellow = addParticles(400);
+let red = addParticles(200);
+let green = addParticles(1000);
 
-function update () {
-    behaviour(yellow, yellow, -0.5,200);
-    behaviour(yellow, red, -0.5,100);
+let yellowTemp = [];
+let redTemp = [];
+let greenTemp = [];
+
+
+function update() {
+
+    yellowTemp = [...yellow];
+    redTemp = [...red];
+    greenTemp = [...green];
+
+    behaviour(yellow, yellow, -0.5);
+    behaviour(yellow, red, -0.5);
     behaviour(yellow, green, 0.8);
 
     behaviour(red, yellow, -0.8);
     behaviour(red, red, -0.8);
-    behaviour(red, green, -1.2);
-    
+    behaviour(red, green, 1.2);
+
     behaviour(green, yellow, 0.3);
     behaviour(green, red, -0.5);
-    behaviour(green, green, 0.01);  
-    
+    behaviour(green, green, 0.01);
 
     con.fillStyle = 'rgba(0, 0, 0, 0.05)';
-    con.fillRect(0, 0, window.innerWidth*scale, window.innerHeight*scale);
+    con.fillRect(0, 0, window.innerWidth * scale, window.innerHeight * scale);
 
-
+    yellow = yellowTemp;
+    red = redTemp;
+    green = greenTemp;
 
     for (let i in yellow) {
         draw(yellow[i].x, yellow[i].y, 'yellow')
@@ -46,40 +73,40 @@ function update () {
 }
 update();
 
-function addParticles (count, mass = 0.5) {
+function addParticles(count, mass = 0.5) {
     let part = [];
-    for (let i = 0; i < count*scale*scale; i++) {
+    for (let i = 0; i < count * scale * scale; i++) {
         part.push({
-            x: Math.random()*can.width|0,
-            y: Math.random()*can.height|0,
+            x: Math.random() * can.width | 0,
+            y: Math.random() * can.height | 0,
             vx: 0,
             vy: 0,
             m: mass
         });
     }
     return part;
-} 
+}
 
-function behaviour (par1, par2, g, d = 80) {
+function behaviour(par1, par2, g, d = 80) {
     for (let i = 0; i < par1.length; i++) {
         let fx = 0;
         let fy = 0;
         let a = null;
         let b = null;
-        for (let j=0; j < par2.length; j++) {
+        for (let j = 0; j < par2.length; j++) {
             a = par1[i];
             b = par2[j];
-            let distancex = a.x-b.x;
-            let distancey = a.y-b.y;
-            let distance = Math.sqrt(distancex*distancex + distancey*distancey);
+            let distancex = a.x - b.x;
+            let distancey = a.y - b.y;
+            let distance = Math.sqrt(distancex * distancex + distancey * distancey);
             if (distance > 0 && distance < d) {
-                let force = g * (a.m+b.m)/distance;
-                fx += force*distancex;
-                fy += force*distancey;
+                let force = g * (a.m + b.m) / distance;
+                fx += force * distancex;
+                fy += force * distancey;
             }
         }
-        a.vx = (a.vx + fx)*0.5;
-        a.vy = (a.vy + fy)*0.5;
+        a.vx = (a.vx + fx) * 0.5;
+        a.vy = (a.vy + fy) * 0.5;
         a.x += a.vx;
         a.y += a.vy;
         if (a.x <= 0) {
@@ -98,3 +125,4 @@ function behaviour (par1, par2, g, d = 80) {
         }
     }
 }
+
